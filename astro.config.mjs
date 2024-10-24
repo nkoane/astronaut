ef3f76b48c73882cf2c84035997c20d61e1db0f2
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2024-10-24 17:19:51 +0200

    Refactor tsconfig.json to extend strictest configuration and add JSX options

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://utlwa-monate.netlify.app',
+  site: 'https://utlwa-monate.netlify.app',
+  integrations: [preact()],
 });
